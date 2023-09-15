c2cbc74fae3e8c4c2a3e325675d6b14e660b4132
Yao Rao <yao.michael.rao@gmail.com>
2023-09-15 14:43:02 -0600

    PROG-27: Set-up initial website

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'progressiveOverflow-Web'`, () => {
+  it(`should have as title 'progressiveOverflow'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('progressiveOverflow-Web');
+    expect(app.title).toEqual('progressiveOverflow');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('progressiveOverflow-Web app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('progressiveOverflow app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { MobileAppComponent } from './mobile-app/mobile-app.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    AboutComponent,
+    MobileAppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
